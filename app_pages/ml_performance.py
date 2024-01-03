842bcd1f985734ec383d0fea6527f80ab969a04c
Renwar-P <139280100+Renwar-P@users.noreply.github.com>
2024-01-03 23:35:50 +0100

    update columns in app_pages\ml_performance.py

--- app_pages/ml_performance.py
+++ app_pages/ml_performance.py
@@ -19,13 +19,14 @@ def page_ml_performance_metrics():
 
 
     st.write("### Model History")
-    col1, col2 = st.columns(2)
-    with col1: 
-        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
-        st.image(model_acc, caption='Model Training Accuracy')
-    with col2:
-        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
-        st.image(model_loss, caption='Model Training Losses')
+
+    st.write(" **Model Training Accuracy**")
+    model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+    st.image(model_acc, caption='Model Training Accuracy')
+    
+    st.write(" **Model Training Losses**")
+    model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+    st.image(model_loss, caption='Model Training Losses')
     st.write("---")
 
     st.write("### Generalised Performance on Test Set")
