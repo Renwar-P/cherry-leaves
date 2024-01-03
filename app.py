c6dcc2c599eb5b08f59c85ea31b3bde727132143
Renwar-P <139280100+Renwar-P@users.noreply.github.com>
2024-01-03 22:31:56 +0100

    removed test folder recursively

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ from app_pages.mildew_detector import page_mildew_detector_body
 from app_pages.project_hypothesis import page_project_hypothesis_body
 from app_pages.ml_performance import page_ml_performance_metrics
 
-app = MultiPage(app_name="Malaria Detector")  # Create an instance of the app
+app = MultiPage(app_name="Cherry Leaves Mildew Detector")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Project Summary", page_summary_body)
 app.add_page("Leaf Visualiser", page_leaf_visualizer_body)
-app.add_page("Malaria Detection", page_mildew_detector_body)
+app.add_page("Mildew Detection", page_mildew_detector_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
 app.add_page("ML Performance Metrics", page_ml_performance_metrics)
 
