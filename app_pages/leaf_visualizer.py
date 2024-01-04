0bb6943fd256720fecba07eeb0a2e860501e95ff
Renwar-P <139280100+Renwar-P@users.noreply.github.com>
2024-01-04 02:53:00 +0100

    update to app_pages\leaf_visualizer.py

--- app_pages/leaf_visualizer.py
+++ app_pages/leaf_visualizer.py
@@ -44,7 +44,7 @@ def page_leaf_visualizer_body():
       labels = os.listdir(my_data_dir+ '/validation')
       label_to_display = st.selectbox(label="Select label", options=labels, index=0)
       if st.button("Create Montage"):      
-            image_montage(dir_path=my_data_dir + '/validation',
+            image_montage(dir_path=my_data_dir + 'validation',
                           label_to_display=label_to_display,
                           nrows=8, ncols=3, figsize=(10, 25))
       st.write("---")
