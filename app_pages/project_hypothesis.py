64502f74d8f169f88566c965996113bda426852b
Renwar-P <139280100+Renwar-P@users.noreply.github.com>
2024-01-04 03:39:44 +0100

    update app_pages\project_hypothesis.py

--- app_pages/project_hypothesis.py
+++ app_pages/project_hypothesis.py
@@ -10,6 +10,6 @@ def page_project_hypothesis_body():
         f"These patterns are typically on the edges of the leaf and differentiate them from a healthy leaf. \n\n"
         f"* An Image Montage shows those typical patterns on the edges. "
         f"Validate the hypothesis by conducting exploratory data analysis (EDA) on the dataset. "
-        f"Visualize samples of healthy and affected cherry leaves to identify any noticeable patterns or differences</span>"
+        f"Visualize samples of healthy and affected cherry leaves to identify any noticeable patterns or differences"
     )
 
