f1ba9399c06737ae42d70efba05d06a5813aec9b
Renwar-P <renwar_86@hotmail.com>
2024-01-05 17:43:28 +0000

    update to app_pages/ml_performance.py

--- app_pages/ml_performance.py
+++ app_pages/ml_performance.py
@@ -2,8 +2,6 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.image import imread
-import sys
-sys.path.append(r'C:\Users\renwa\OneDrive\Desktop\Django3blog\cherry-leaves')
 
 from src.machine_learning.evaluate_clf import load_test_evaluation
 
